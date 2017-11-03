2fd4ba3fae7bd013501d03580bae00b4215fe81f
Yusuf Zerdazi <yusuf@zerdazi.com>
2017-11-03 21:32:49 +0000

    Initial project.

---
+++ yusuf.zerdazi.com/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace yusuf.zerdazi.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
