faf999c2c2a06a2bc84afba5585cf908d9736ad2
Yusuf Zerdazi <yusuf@zerdazi.com>
2017-11-05 12:16:27 +0000

    Mirrored current website.

--- Yusuf.Zerdazi.Web/wwwroot/js/header.js
+++ Yusuf.Zerdazi.Web/wwwroot/js/header.js
@@ -1,71 +1,90 @@
 var yoff = 0.0;        // 2nd dimension of perlin noise
 var isOverCircle;
+var leftColour = [231, 66, 53];
+var rightColour = [250, 187, 9];
+var bgColour = 255;
+var imgUrl = window.location.origin + "/images/colour.gif";
 
-function setup() {
-  canvas = createCanvas($('body').innerWidth(), 100);
-  img = loadImage("images/header.gif");
-  canvas.parent('header_wave');
+if (window.location.href.includes("everydays")) {
+    var leftColour = [255, 255, 255];
+    var rightColour = [255, 255, 255];
+    var bgColour = 0;
+    var imgUrl = window.location.origin + "/images/header.gif";
 }
 
-function magnitude(x){
-  var half = width / 2;
-  if(x < half){
-    return max(0, (half - 110) - x)/half;
-  } else {
-    return max(0, x - half - 110)/half;
-  }
+function setup() {
+    canvas = createCanvas($('body').innerWidth(), 100);
+    img = loadImage(imgUrl);
+    canvas.parent('header_wave');
+}
+
+function magnitude(x) {
+    var half = width / 2;
+    if (x < half) {
+        return max(0, (half - 110) - x) / half;
+    } else {
+        return max(0, x - half - 110) / half;
+    }
 }
 
 function draw() {
-  background(255);
-  noStroke();
-  fill(0);
-  beginShape();
-  var xoff = 0;
+    background(bgColour);
+    fill(leftColour);
+    noStroke();
+    beginShape();
+    var xoff = 0;
 
-  for (var x = 0; x < width+10; x += 10) {
-    if(x > width){
-      x = width;
+    for (var x = 0; x < width / 2; x += 10) {
+        var y = map(1.5 * magnitude(x) * (noise(xoff, yoff) - 0.5), 0, 1, 200, 300) - 150;
+        vertex(x, y);
+        xoff += 0.05;
     }
-    var y = map(1.5 * magnitude(x) * (noise(xoff, yoff) - 0.5), 0, 1, 200,300) - 150;
+    vertex(width / 2, 0);
+    vertex(0, 0);
+    endShape(CLOSE);
 
-    vertex(x, y);
-    xoff += 0.05;
-  }
-  yoff += 0.01;
-  vertex(width, height);
-  vertex(0, height);
-  endShape(CLOSE);
-  image(img, width/2-50, 0, 100, 100);
+    fill(rightColour);
+    beginShape();
+    for (var x = width / 2; x < width + 10; x += 10) {
+        if (x > width) {
+            x = width;
+        }
+        var y = map(1.5 * magnitude(x) * (noise(xoff, yoff) - 0.5), 0, 1, 200, 300) - 150;
+        vertex(x, y);
+        xoff += 0.05;
+    }
+    vertex(width, 0);
+    vertex(width / 2, 0);
+    endShape(CLOSE);
 
-  // get distance between mouse and circle
-  var distance = dist(mouseX, mouseY, width/2, 50);
+    yoff += 0.01;
+    image(img, width / 2 - 50, 0, 100, 100);
 
-  // if the distance is less than the circle's radius
-  if(distance < 50)
-  {
-    isOverCircle = true;
-  } else {
-    isOverCircle = false;
-  }
-  // draw a circle
-  fill(0, 0, 0, 1);
-  if(isOverCircle == true)
-  {
-    cursor(HAND);
-  } else {
-    cursor(ARROW);
-  }
+
+    // get distance between mouse and circle
+    var distance = dist(mouseX, mouseY, width / 2, 50);
+
+    // if the distance is less than the circle's radius
+    if (distance < 50) {
+        isOverCircle = true;
+    } else {
+        isOverCircle = false;
+    }
+    // draw a circle
+    fill(0, 0, 0, 1);
+    if (isOverCircle == true) {
+        cursor(HAND);
+    } else {
+        cursor(ARROW);
+    }
 }
 
-function mousePressed()
-{
-  if(isOverCircle == true)
-  {
-    window.open("../","_self")
-  }
+function mousePressed() {
+    if (isOverCircle == true) {
+        window.open(window.location.origin, "_self")
+    }
 }
 
-$(window).resize(function(){
+$(window).resize(function () {
     canvas.size($('body').innerWidth(), 100);
 });
