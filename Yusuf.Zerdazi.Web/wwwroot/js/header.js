5aa0af3ffa11e7a05e056dd691cfd0d7f1c3e99e
Yusuf Zerdazi <yusuf@zerdazi.com>
2017-12-09 22:42:47 +0000

    Added new header.

--- Yusuf.Zerdazi.Web/wwwroot/js/header.js
+++ Yusuf.Zerdazi.Web/wwwroot/js/header.js
@@ -1,71 +1,95 @@
 var yoff = 0.0;        // 2nd dimension of perlin noise
 var isOverCircle;
-var leftColour = [231, 66, 53];
-var rightColour = [250, 187, 9];
-var bgColour = 255;
-var imgUrl = window.location.origin + "/images/colour.gif";
+var leftColour = [255, 255, 255];
+var rightColour = [255, 255, 255];
+var bgColour = [255, 255, 255];
+var imgUrl = window.location.origin + "/images/tile_centre_small.png";
+var mainUrl = window.location.origin + "/images/tile_main_small.png";
+var joinUrl = window.location.origin + "/images/tile_join_small.png";
 
 if (window.location.href.includes("everydays")) {
-    var leftColour = [255, 255, 255];
-    var rightColour = [255, 255, 255];
-    var bgColour = 0;
-    var imgUrl = window.location.origin + "/images/header.gif";
+    bgColour = 0;
 }
 
 function setup() {
-    canvas = createCanvas($('body').innerWidth(), 100);
+    canvas = createCanvas($('body').innerWidth(), 150);
+
     img = loadImage(imgUrl);
+    mainImg = loadImage(mainUrl);
+    joinImg = loadImage(joinUrl);
     canvas.parent('header_wave');
+
+    img_map = {
+        0: mainImg,
+        1: joinImg,
+    }
 }
 
 function magnitude(x) {
     var half = width / 2;
     if (x < half) {
-        return max(0, (half - 110) - x) / half;
+        return max(0, (half - 20) - x) / half;
     } else {
-        return max(0, x - half - 110) / half;
+        return max(0, x - half - 20) / half;
+    }
+}
+
+
+
+var width_map = {
+    0: 150,
+    1: 23.33
+}
+
+function drawTiledImage() {
+    var x = width / 2 - 75;
+    var img_enum = 0;
+    image(img, x, 0, 150, 150);
+    x += width_map[img_enum] - 1;
+    while (x < width) {
+        image(img_map[img_enum], x, 0, width_map[img_enum], 150);
+        x += width_map[img_enum] - 1;
+        img_enum = (img_enum + 1) % 2
+    }
+
+    var x = width / 2 - 224;
+    var img_enum = 0;
+    while (x + width_map[img_enum] > -width) {
+        image(img_map[img_enum], x, 0, width_map[img_enum], 150);
+        x -= width_map[(img_enum+1) % 2] - 1;
+        console.log(x);
+        img_enum = (img_enum + 1) % 2
     }
 }
 
 function draw() {
     background(bgColour);
-    fill(leftColour);
+    fill(bgColour);
     noStroke();
     beginShape();
+
+
+    drawTiledImage();
+
     var xoff = 0;
 
-    for (var x = 0; x < width / 2; x += 10) {
-        var y = map(1.5 * magnitude(x) * (noise(xoff, yoff) - 0.5), 0, 1, 200, 300) - 150;
+    for (var x = 0; x < width + 10; x += 10) {
+        var y = map(0.75 * magnitude(x) * (noise(xoff, yoff) - 1), 0, 1, 200, 300) - 50;
         vertex(x, y);
         xoff += 0.05;
     }
-    vertex(width / 2, 0);
-    vertex(0, 0);
-    endShape(CLOSE);
-
-    fill(rightColour);
-    beginShape();
-    for (var x = width / 2; x < width + 10; x += 10) {
-        if (x > width) {
-            x = width;
-        }
-        var y = map(1.5 * magnitude(x) * (noise(xoff, yoff) - 0.5), 0, 1, 200, 300) - 150;
-        vertex(x, y);
-        xoff += 0.05;
-    }
-    vertex(width, 0);
-    vertex(width / 2, 0);
+    vertex(width, 150);
+    vertex(0, 150);
     endShape(CLOSE);
 
     yoff += 0.01;
-    image(img, width / 2 - 50, 0, 100, 100);
-
+    
 
     // get distance between mouse and circle
-    var distance = dist(mouseX, mouseY, width / 2, 50);
+    var distance = dist(mouseX, mouseY, width / 2, 75);
 
     // if the distance is less than the circle's radius
-    if (distance < 50) {
+    if (distance < 75) {
         isOverCircle = true;
     } else {
         isOverCircle = false;
@@ -86,5 +110,5 @@ function mousePressed() {
 }
 
 $(window).resize(function () {
-    canvas.size($('body').innerWidth(), 100);
+    canvas.size($('body').innerWidth(), 150);
 });
