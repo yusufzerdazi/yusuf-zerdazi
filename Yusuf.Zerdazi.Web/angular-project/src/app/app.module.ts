93bbb870b6907f2b135613b43382b00e3a058a99
Yusuf Zerdazi <yusuf@zerdazi.com>
2018-01-03 18:31:16 +0000

    Don't use media elements, use videogular.

--- Yusuf.Zerdazi.Web/angular-project/src/app/app.module.ts
+++ Yusuf.Zerdazi.Web/angular-project/src/app/app.module.ts
@@ -1,24 +1,29 @@
+// Angular modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+// Npm modules
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+import { VgBufferingModule } from 'videogular2/buffering';
+
+// Custom components
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header.component';
 import { FooterComponent } from './shared/footer.component';
-
 import { EverydaysComponent } from './everydays/everydays.component';
 import { HomeComponent } from './home/home.component';
 import { MonthComponent } from './everydays/month.component';
 import { EverydayComponent } from './everydays/everyday.component';
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
   declarations: [
@@ -32,16 +37,20 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule,
     NgbTooltipModule,
     InfiniteScrollModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: 'everydays', component: EverydaysComponent },
       { path: '', component: HomeComponent, pathMatch: 'full'}
-    ], {useHash: true})
+    ], {useHash: true, initialNavigation: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
