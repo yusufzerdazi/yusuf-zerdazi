70c023ff0f5b6f7c9c46a56f73950fd1708419d5
Yusuf Zerdazi <yusuf@zerdazi.com>
2021-01-31 14:55:32 +0000

    Some improvements

--- src/components/camera/index.js
+++ src/components/camera/index.js
@@ -39,15 +39,15 @@ class Camera extends React.Component {
         <p>There's numerous home security systems available, most of which are at least a hundred pounds. However, with a simple Raspberry Pi and a camera, it's possible to achieve a similar result for less than fifty.</p>
         <p>The Raspberry Pi streams its live camera input, and uses a package called <a href="https://motion-project.github.io/">Motion</a> to detect if anything moves. If it does, it calls a webhook which gives me a phone notification through <a href="https://ifttt.com/">IFTTT</a>. 
         This notification gives me a link to view the camera live-stream, so I can see what the camera has detected.</p>
-        <p>Here's a <a href="https://pi.zerdazi.com:8081">live stream</a> of my bird feeder:</p>
+        <h3>Here's a <a href="https://pi.zerdazi.com:8081">live stream</a> of my bird feeder:</h3>
         <div className="stream-container-container">
           <div className="stream-container">
             <img className={styles.streamImage} src="https://pi.zerdazi.com:8081"></img>
           </div>
         </div>
-        <h2>Recent Captures</h2>
+        <h3>Recent Captures</h3>
         { this.state.blobs ? <div className="row">{this.state.blobs.map((blob, i) => {
-          return <div key={i} className="col-md-4 col-sm-6"><video className="capture" width="100%" height="100%" controls src={blob.url}></video></div>
+          return <div key={i} className="col-md-4 col-sm-6 col-6"><video className="capture" width="100%" controls src={blob.url}></video></div>
         })}</div> : <span><i className="fas fa-spinner fa-pulse fa-2x fa-fw" color="white"/></span> }
       </div>
   }
