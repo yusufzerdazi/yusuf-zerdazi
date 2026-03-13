f15d84468572de429c6fe8da42448a728a0137f2
Yusuf Zerdazi <yusuf@zerdazi.com>
2026-03-13 07:59:28 +0000

    decom update

--- src/Home.tsx
+++ src/Home.tsx
@@ -178,7 +178,7 @@ const portfolioSections: PortfolioSections = {
             title: "LED Screen",
             description: "An AI powered light installation for a 'burning man' event in London called Decompression, which constructs uniquely generated visuals on an LED matrix in response both to the words spoken to it and music played nearby.\n\nFull build video coming soon!",
             yearRange: { start: 2023, end: 2025 },
-            videos: ["./decom_1.mp4", "./decom_2.mp4"],
+            videos: ["./decom_3.mp4", "./decom_4.mp4", "./decom_1.mp4", "./decom_2.mp4"],
             links: [
               { name: "GitHub", url: "https://github.com/yusufzerdazi/led-screen", icon: "fab fa-github" }
             ]
@@ -189,15 +189,15 @@ const portfolioSections: PortfolioSections = {
     title: "Project: Magic Mirror",
     icon: <i className="fas fa-magic"></i>,
     projects: [
-        {
-            title: "Magic Mirror",
-            description: "An interactive 'magic mirror' created for Mischief (a music event series), whereby a wall mounted screen with camera input displays a transformed, psychedelic reflection of the observers.",
-            yearRange: { start: 2025, end: 2025 },
-            videos: ["./mirror_1.mp4", "./mirror_2.mp4", "./mirror_3.mp4"],
-            links: [
-              { name: "GitHub", url: "https://github.com/yusufzerdazi/magicmirror", icon: "fab fa-github" }
-            ]
-        }
+         {
+             title: "Magic Mirror",
+             description: "An interactive display created for Mischief (a music event series), featuring psychedelic visual effects and animations on a wall-mounted screen.",
+             yearRange: { start: 2025, end: 2025 },
+             videos: ["./mirror_1.mp4", "./mirror_2.mp4", "./mirror_3.mp4"],
+             links: [
+               { name: "GitHub", url: "https://github.com/yusufzerdazi/magicmirror", icon: "fab fa-github" }
+             ]
+         }
     ]
   },
   "Car": {
